52a317f7050a64703dea856d2b70d023decde9fa
Stanzione <jvslisboa@poli.ufrj.br>
2021-03-04 23:44:17 -0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,5 +6,9 @@ app.get('/projects', (request, response) => {
     return  response.json({ message : "Hey guys, this is how that you start a basic backend haha!"});
 } );
 
-app.listen(3333);
+app.listen(3333, () => {
+
+    console.log ('Back-end started 😎');
+
+});
 
