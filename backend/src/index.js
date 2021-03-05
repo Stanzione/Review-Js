74bf9320683d55b33f05715a403a23ef04ff44a2
Stanzione <jvslisboa@poli.ufrj.br>
2021-03-05 00:07:50 -0300

    separando rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,45 @@
+const { request, response } = require('express');
 const express = require('express'); 
 
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return  response.json({ message : "Hey guys, this is how that you start a basic backend haha!"});
+    return  response.json([
+
+        'Project 1',
+        'Project 2'
+
+    ]);
 } );
+
+app.post('/projects', (request, response) => {
+    
+    return response.json([
+        'Project 1',
+        'Project 2',
+        'Project 3',
+    ])
+
+})
+
+app.put('/projects/:id', (request, response) => {
+    
+    return response.json([
+        'Project 4',
+        'Project 2',
+        'Project 3',
+    ])
+
+})
+
+app.delete('/projects/:id', (request, response) => {
+    
+    return response.json([
+        'Project 2',
+        'Project 3',
+    ])
+
+})
 
 app.listen(3333, () => {
 
