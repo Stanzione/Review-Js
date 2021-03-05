f3c5f416d7fb6f27ebb871e8964ea97104872a1a
Stanzione <jvslisboa@poli.ufrj.br>
2021-03-04 23:25:38 -0300

    first example of a simple backend

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+    return  response.json({ message : "Hey guys, this is how that you start a basic backend haha!"});
+} );
+
+app.listen(3333);
+
